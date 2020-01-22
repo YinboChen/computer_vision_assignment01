31375f06e2180bc5adc0dca3cb0fa315e57ec863
Yinbo Chen <39783798+YinboChen@users.noreply.github.com>
2020-01-22 07:57:34 -0700

    added meanfilter

--- meanFilter.m
+++ meanFilter.m
@@ -1,4 +1,10 @@
 function [outimg] = meanFilter( inImg, kernel_size)
 
+inImg = imread('sully.bmp');
+M = imnoise(inImg,'salt & pepper');
+imshow(M);
+% M(x,y)= sum(sum(A(x-r:x+r,y-r:y+r)))
+%M(pixel)=box sum value/(r*r)
+
 
 end
