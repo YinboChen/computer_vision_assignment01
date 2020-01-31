be126a327db2a49c8f11fa4082cd96111a3a4920
Yinbo <yinbo@cu-tcom-4-10.203.29.95.int.colorado.edu>
2020-01-31 13:36:13 -0700

    optimised addRandomNoise functions

--- addRandomNoise_NL.m
+++ addRandomNoise_NL.m
@@ -1,5 +1,6 @@
 function [ outImg] = addRandomNoise_NL( inImg)
     inImg = double(inImg);
-    R = randi([-255 255],size(inImg,1),size(inImg,2));
+    R = randi([-255 255],size(inImg,1),size(inImg,2),3);
     outImg = inImg + R;
+    outImg = uint8(outImg);
 end
